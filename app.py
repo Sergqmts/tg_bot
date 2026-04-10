219ee9b4788ffb160fc070ae4b9208536c9c8290
a1234 <sergqmts@gmail.com>
2026-04-10 20:44:23 +0300

    Fix user_reposts iteration

--- app.py
+++ app.py
@@ -536,10 +536,12 @@ def delete(post_id):
 def user_profile(username):
     user = User.query.filter_by(username=username).first_or_404()
     posts = user.posts.order_by(Post.created_at.desc()).all()
-    user_reposts = Repost.query.filter_by(user_id=user.id).order_by(Repost.created_at.desc()).all() if user.id else []
+    user_reposts = []
+    if user.id:
+        user_reposts = Repost.query.filter_by(user_id=user.id).order_by(Repost.created_at.desc()).all()
     repost_counts = {}
     for p in posts:
-        repost_counts[p.id] = Repost.query.filter_by(post_id=p.id).count()
+        repost_counts[p.id] = Repost.query.filter_by(post_id=p.id).count() if p.id else 0
     is_following = current_user.is_authenticated and current_user.is_following(user)
     return render_template('profile.html', user=user, posts=posts, user_reposts=user_reposts, repost_counts=repost_counts, is_following=is_following)
 
