bf38c0594a1d2d0a82ae48942ea78ef6941be61c
a1234 <sergqmts@gmail.com>
2026-04-17 19:36:29 +0300

    Fix relationship foreign keys

--- app.py
+++ app.py
@@ -302,8 +302,6 @@ class User(UserMixin, db.Model):
     
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     likes = db.relationship('Like', backref='user', lazy='dynamic')
-    comments = db.relationship('Comment', backref='author', lazy='dynamic', foreign_keys='Comment.user_id')
-    
     followed = db.relationship(
         'User', secondary=followers,
         primaryjoin=(followers.c.follower_id == id),
@@ -481,7 +479,7 @@ class Post(db.Model):
     community_id = db.Column(db.Integer, db.ForeignKey('community.id'), nullable=True)
     is_community_post = db.Column(db.Boolean, default=False)
     likes = db.relationship('Like', backref='post', lazy='dynamic', cascade='all, delete-orphan')
-    comments = db.relationship('Comment', backref='post', lazy='dynamic', cascade='all, delete-orphan', foreign_keys='Comment.post_id')
+    comments = db.relationship('Comment', foreign_keys=[Comment.post_id], lazy='dynamic', cascade='all, delete-orphan')
     media = db.relationship('Media', backref='post', lazy='dynamic', cascade='all, delete-orphan')
 
     def liked_by(self, user):
